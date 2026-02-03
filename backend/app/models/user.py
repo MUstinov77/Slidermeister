9bf282f5f817e6a1262c649838910f33fc4eb666
MUstinov77 <m.ustinov2001@yandex.ru>
2026-02-03 15:10:44 +0300

    init

---
+++ backend/app/models/user.py
@@ -0,0 +1,24 @@
+import uuid
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import text, String
+
+from backend.app.models.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"  # noqa
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text("gen_random_uuid()"),
+    )
+    username: Mapped[str] = mapped_column(
+        String(),
+        unique=True,
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String(),
+    )
