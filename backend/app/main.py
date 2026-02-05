d75abfab91a1d62aad3ecfd64e8efe882aba23ca
Max Ustinov <m.ustinov2001@yandex.ru>
2026-02-05 21:02:40 +0300

    change config, change main.py, add asyncpg in requ

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from backend.app.core.config import get_settings
 
 settings = get_settings()
 
+app = create_app(settings)
 
 if __name__ == "__main__":
-    app = create_app(settings)
     uvicorn.run(app, host="0.0.0.0", port=8000)
